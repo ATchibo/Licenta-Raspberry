77ea786ad8c15610e9cef74455087468e6316e2f
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-11-15 15:28:04 +0200

    90% home page design, more tweaks after be

--- components/homepage/watering_options_view.py
+++ components/homepage/watering_options_view.py
@@ -2,6 +2,7 @@ from kivy.clock import Clock
 from kivy.garden.matplotlib import FigureCanvasKivyAgg
 from kivy.lang import Builder
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.menu import MDDropdownMenu
 from matplotlib import pyplot as plt
 
 
@@ -16,5 +17,30 @@ class WateringOptionsView(MDBoxLayout):
         Clock.schedule_once(self.init, 0.1)
 
     def init(self, *args):
-        print("rahat")
+        self.init_dropdown(*args)
+
+    def init_dropdown(self, *args):
+
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": f"Item {i}",
+                "on_release": lambda x=f"Item {i}": self.set_item(x),
+            } for i in range(5)
+        ]
+
+        self.menu = MDDropdownMenu(
+            caller=self.ids.drop_item,
+            items=menu_items,
+            position="center",
+            width_mult=2,
+        )
+
+        self.menu.bind()
+
+    def set_item(self, text_item):
+        self.ids.drop_item.set_item(text_item)
+        self.menu.dismiss()
+
+
 
