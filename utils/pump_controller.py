d6bbb087cb83f0013af214b31dbf400b7808c1ad
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-12-28 15:09:05 +0200

    moisture controller works

--- utils/pump_controller.py
+++ utils/pump_controller.py
@@ -1,6 +1,6 @@
 import time
 
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 class PumpController:
@@ -11,32 +11,32 @@ class PumpController:
         self.pin = pin
         # set the liters per second
         self.pump_capacity = liters_per_second
-    #
-    #     GPIO.setwarnings(False)
-    #     # set the mode to BCM - refer to the pins by the GPIO number
-    #     GPIO.setmode(GPIO.BCM)
-    #     # set the pin to output
-    #     GPIO.setup(self.pin, GPIO.OUT)
-    #     # set the pin to low - not sending any power
-    #     GPIO.output(self.pin, GPIO.LOW)
-    #
-    # def start_watering_for_seconds(self, seconds):
-    #     """Start watering for the specified amount of seconds."""
-    #
-    #     time_end = time.time() + seconds
-    #     while time.time() < time_end:
-    #         self._start()
-    #     self._stop()
-    #
-    # def start_watering_for_liters(self, liters):
-    #     """Start watering for the specified amount of minutes."""
-    #     seconds = liters / self.pump_capacity
-    #     self.start_watering_for_seconds(seconds)
-    #
-    # def _start(self):
-    #     """Start the pump."""
-    #     GPIO.output(self.pin, GPIO.HIGH)
-    #
-    # def _stop(self):
-    #     """Stop the pump."""
-    #     GPIO.output(self.pin, GPIO.LOW)
+
+        GPIO.setwarnings(False)
+        # set the mode to BCM - refer to the pins by the GPIO number
+        GPIO.setmode(GPIO.BCM)
+        # set the pin to output
+        GPIO.setup(self.pin, GPIO.OUT)
+        # set the pin to low - not sending any power
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def start_watering_for_seconds(self, seconds):
+        """Start watering for the specified amount of seconds."""
+
+        time_end = time.time() + seconds
+        while time.time() < time_end:
+            self._start()
+        self._stop()
+
+    def start_watering_for_liters(self, liters):
+        """Start watering for the specified amount of minutes."""
+        seconds = liters / self.pump_capacity
+        self.start_watering_for_seconds(seconds)
+
+    def _start(self):
+        """Start the pump."""
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def _stop(self):
+        """Stop the pump."""
+        GPIO.output(self.pin, GPIO.LOW)
