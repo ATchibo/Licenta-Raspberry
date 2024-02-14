011ee7a0c9e188c06b283ff1be6a14737acda805
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-02-14 20:31:36 +0200

    reimported code

--- utils/moisture_controller.py
+++ utils/moisture_controller.py
@@ -1,9 +1,9 @@
-# from gpiozero import MCP3008
+from gpiozero import MCP3008
 
 
 class MoistureController:
     def __init__(self, channel):
-        # self.sensor = MCP3008(channel=channel)
+        self.sensor = MCP3008(channel=channel)
 
         # the absolute values for dry and wet recorded from the sensor
         self.absolute_dry = 0.7518319491939423
@@ -11,9 +11,9 @@ class MoistureController:
         self.interval = self.absolute_dry - self.absolute_wet
 
     def get_moisture(self):
-        pass
-        # return self.sensor.value
+        # pass
+        return self.sensor.value
 
     def get_moisture_percentage(self):
-        pass
-        # return round(1 - (self.get_moisture() - self.absolute_wet) / self.interval, 2) * 100
+        # pass
+        return round(1 - (self.get_moisture() - self.absolute_wet) / self.interval, 2) * 100
