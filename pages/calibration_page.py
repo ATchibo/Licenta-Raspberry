d01b504f011aa100c732cf3372527888dbdf3253
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-03-25 11:15:10 +0200

    started adding calibration page

---
+++ pages/calibration_page.py
@@ -0,0 +1,18 @@
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+
+Builder.load_file("pages/calibration_page.kv")
+
+
+class CalibrationPage(MDScreen):
+    def __init__(self, **kwargs):
+        super(CalibrationPage, self).__init__(**kwargs)
+
+    def on_enter(self, *args):
+        pass
+
+    def on_leave(self, *args):
+        pass
+
+    def start_moisture_calibration(self, *args):
+        pass
