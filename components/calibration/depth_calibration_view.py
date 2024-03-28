1343f6e5e2d87cc3fe7628a4585cc241b74f92ad
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-03-28 17:44:36 +0200

    started building the calibration page

--- components/calibration/depth_calibration_view.py
+++ components/calibration/depth_calibration_view.py
@@ -7,14 +7,14 @@ from kivymd.uix.screen import MDScreen
 
 from utils.local_storage_controller import LocalStorageController
 from utils.raspberry_controller import RaspberryController
+from utils.water_depth_measurement_controller import WaterDepthMeasurementController
 
 Builder.load_file("components/calibration/depth_calibration_view.kv")
 
 
 class DepthCalibrationView(MDScreen):
     calibrating = BooleanProperty(False)
-    step_text = StringProperty("Step 1: Place the sensor in the air"
-                               " and press the button below")
+    step_text = StringProperty("Step 1: Empty the water tank and press the button below")
     loading_text = StringProperty("")
     calibrate_button_text = StringProperty("Calibrate")
 
@@ -23,21 +23,20 @@ class DepthCalibrationView(MDScreen):
 
         self._min_value = None
         self._max_value = None
-
-        self._step_time_s = 10
+        self._tank_volume = None
 
         self._current_thread = None
         self._cancel_event = threading.Event()
 
     def on_enter(self, *args):
         self.calibrating = False
-        self.step_text = ("Step 1: Place the sensor in the air"
-                          " and press the button below")
+        self.step_text = "Step 1: Empty the water tank and press the button below"
         self.loading_text = ""
         self.calibrate_button_text = "Calibrate"
 
         self._min_value = None
         self._max_value = None
+        self._tank_volume = None
 
         self._current_thread = None
         self._cancel_event.clear()
@@ -46,16 +45,7 @@ class DepthCalibrationView(MDScreen):
         self.on_enter(args)
 
     def calibrate_button_function(self, *args):
-        if self._min_value is None:
-            self.calibrating = True
-            self._current_thread = threading.Thread(
-                target=self._get_min_value,
-                args=(self._on_min_value_thread_finished,)
-            )
-
-            self._current_thread.start()
-
-        elif self._max_value is None:
+        if self._max_value is None:
             self.calibrating = True
             self._current_thread = threading.Thread(
                 target=self._get_max_value,
@@ -64,50 +54,35 @@ class DepthCalibrationView(MDScreen):
 
             self._current_thread.start()
 
-        else:
-            LocalStorageController().set_moisture_sensor_absolute_values(
-                self._min_value,
-                self._max_value
+        elif self._min_value is None:
+            self.calibrating = True
+            self._current_thread = threading.Thread(
+                target=self._get_min_value,
+                args=(self._on_min_value_thread_finished,)
             )
 
-            RaspberryController().moisture_controller.absolute_dry = self._max_value
-            RaspberryController().moisture_controller.absolute_wet = self._min_value
+            self._current_thread.start()
+        else:
+            # self._tank_volume = RaspberryController().moisture_controller.get_moisture()
+            # RaspberryController().moisture_controller.tank_volume = self._tank_volume
+            #
+            # LocalStorageController().set_moisture_sensor_absolute_values(
+            #     self._min_value,
+            #     self._max_value
+            # )
+            #
+            # RaspberryController().moisture_controller.absolute_dry = self._max_value
+            # RaspberryController().moisture_controller.absolute_wet = self._min_value
 
             self._navigate_back()
 
     def _get_min_value(self, on_thread_finished=None):
-        _time = 0
-
-        while _time < self._step_time_s:
-            self.loading_text = f"Time left: {self._step_time_s - _time} seconds"
-            _time += 1
-
-            self._cancel_event.wait(1)
-            if self._cancel_event.is_set():
-                return
-
-            _sensor_value = RaspberryController().moisture_controller.get_moisture()
-            if self._min_value is None or _sensor_value < self._min_value:
-                self._min_value = _sensor_value
-
+        self._min_value = WaterDepthMeasurementController().measure_water_depth_cm()
         if on_thread_finished is not None:
             on_thread_finished()
 
     def _get_max_value(self, on_thread_finished=None):
-        _time = 0
-
-        while _time < self._step_time_s:
-            self.loading_text = f"Time left: {self._step_time_s - _time} seconds"
-            _time += 1
-
-            self._cancel_event.wait(1)
-            if self._cancel_event.is_set():
-                return
-
-            _sensor_value = RaspberryController().moisture_controller.get_moisture()
-            if self._max_value is None or _sensor_value > self._max_value:
-                self._max_value = _sensor_value
-
+        self._max_value = WaterDepthMeasurementController().measure_water_depth_cm()
         if on_thread_finished is not None:
             on_thread_finished()
 
@@ -123,13 +98,12 @@ class DepthCalibrationView(MDScreen):
         Clock.schedule_once(_aux, 0.1)
 
     def _on_min_value_thread_finished(self):
-        self.step_text = ("Step 2: Place the sensor in the water"
-                          " and press the button below")
-        self.calibrating = False
-        self.loading_text = ""
-
-    def _on_max_value_thread_finished(self):
-        self.step_text = "Calibration finished"
+        self.step_text = "Step 3: Specify the tank volume in L and press Save\n"
         self.calibrating = False
         self.loading_text = ""
         self.calibrate_button_text = "Save"
+
+    def _on_max_value_thread_finished(self):
+        self.step_text = "Step 2: Fill the water tank and press the button below"
+        self.calibrating = False
+        self.loading_text = ""
